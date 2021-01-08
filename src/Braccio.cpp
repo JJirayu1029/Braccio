115d7c4e0c0d8056230364f9b9f8fab3e321d958
JJirayu1029 <74522861+JJirayu1029@users.noreply.github.com>
2021-01-08 08:51:23 +0700

    Update Braccio.cpp

--- src/Braccio.cpp
+++ src/Braccio.cpp
@@ -63,8 +63,13 @@ unsigned int _Braccio::begin(int soft_start_level) {
 	wrist_ver.attach(5);
 	gripper.attach(3);
         
-	Braccio.ServoMovement(20, 170,  140, 0, 180,  180,   95);
-	
+	//For each step motor this set up the initial degree
+	base.write(170);
+	shoulder.write(140);
+	elbow.write(0);
+	wrist_ver.write(180);
+	wrist_rot.write(180);
+	gripper.write(95);
 	//Previous step motor position
 	step_base = 170;
 	step_shoulder = 140;
